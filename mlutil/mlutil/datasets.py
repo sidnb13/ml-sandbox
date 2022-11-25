1898bbe04e5faeecc872c9ab41340360fd3fa858
Sidharth Baskaran <sidnbaskaran@gmail.com>
2022-11-24 18:18:55 -0600

    Add local convenience package

---
+++ mlutil/mlutil/datasets.py
@@ -0,0 +1,45 @@
+'''
+Fashion MNIST dataset convenience wrapper.
+'''
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torchvision.datasets import FashionMNIST
+import matplotlib.pyplot as plt
+
+class FashionMNISTSet:
+    def __init__(self, dim=(28,28), batch_size=32) -> None:
+        composition = transforms.Compose([transforms.Resize(dim), transforms.ToTensor()])
+        self.train_set = FashionMNIST(root='./dataset', train=True, download=True, transform=composition)
+        self.val_set = FashionMNIST(root='./dataset', train=False, download=True, transform=composition)
+        self.batch_size = batch_size
+        self.dim = dim
+        
+    def create_dataloader(self, train: bool):
+        """Create a dataloader."""
+        if train:
+            shuffle = True
+            dataset = self.train_set
+        else:
+            shuffle = False
+            dataset = self.val_set
+        return torch.utils.data.DataLoader(dataset, batch_size=self.batch_size, shuffle=shuffle)
+    
+    def text_labels(self, indices):
+        """Return text labels."""
+        labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+        return [labels[int(i)] for i in indices]
+    
+    def visualize(self, batch, nrows=1, ncols=8, labels=[]):
+        X, y = batch
+        if not labels:
+            labels = self.text_labels(y)
+
+        _, axes = plt.subplots(nrows=nrows, ncols=ncols, figsize=(8,8))
+        
+        for i, ax in enumerate(axes.flat):
+            ax.imshow(X[i].squeeze(0), cmap='gray')
+            ax.set_title(labels[i])
+            ax.axis('off')
---
+++ mlutil/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='mlutil',
+    version='0.1',
+    packages=['mlutil']
+)
