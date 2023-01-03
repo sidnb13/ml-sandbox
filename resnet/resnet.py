95ade484b083cfe8afd68ac8c7f09157b088cd2e
Sidharth Baskaran <sidnbaskaran@gmail.com>
2023-01-02 19:06:04 -0600

    Add ResNet main arch

--- resnet/resnet.py
+++ resnet/resnet.py
@@ -13,6 +13,7 @@ from absl import app
 from absl import flags
 from absl import logging
 import config
+from typing import *
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string("data_dir", "./data/mnist", "Directory to download data to.")
@@ -27,21 +28,27 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1) -> None:
+    def __init__(
+        self, in_channels, pre_expansion_channels, stride=1, use_batchnorm=True, expansion=1
+    ) -> None:
         super().__init__()
+        self.expansion = expansion
 
         self.conv1 = nn.Conv2d(
-            in_channels, out_channels, kernel_size=3, stride=stride, padding=1
+            in_channels, pre_expansion_channels, kernel_size=3, stride=stride, padding=1
         )
+        self.bn1 = nn.BatchNorm2d(pre_expansion_channels) if use_batchnorm else nn.Identity()
+
         self.conv2 = nn.Conv2d(
-            out_channels, out_channels, kernel_size=3, stride=1, padding=1
+            in_channels, pre_expansion_channels, kernel_size=3, stride=1, padding=1
         )
+        self.bn2 = nn.BatchNorm2d(pre_expansion_channels) if use_batchnorm else nn.Identity()
 
         self.relu = nn.ReLU()
 
-        if in_channels != out_channels:
+        if in_channels != pre_expansion_channels * expansion:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride),
+                nn.Conv2d(pre_expansion_channels, pre_expansion_channels * expansion, kernel_size=1, stride=stride),
             )
         else:
             self.shortcut = nn.Identity()
@@ -53,19 +60,30 @@ class ResidualBlock(nn.Module):
 
         # residual connection
         out += self.shortcut(x)
-
         return self.relu(out)
 
 
 class Bottleneck(nn.Module):
     def __init__(
-        self, in_channels, pre_expansion_channels, stride=1, expansion=4
+        self,
+        in_channels,
+        pre_expansion_channels,
+        stride=1,
+        use_batchnorm=True,
+        expansion=4,
     ) -> None:
         super().__init__()
+        self.expansion = expansion
 
-        self.conv1 = nn.Conv2d(
-            in_channels, pre_expansion_channels, kernel_size=1, stride=1, padding=1
+        # No batchnorm applied to the first conv layer
+        self.conv1 = (
+            nn.Conv2d(
+                in_channels, pre_expansion_channels, kernel_size=1, stride=1, padding=1
+            )
+            if in_channels != pre_expansion_channels
+            else nn.Identity()
         )
+
         self.conv2 = nn.Conv2d(
             pre_expansion_channels,
             pre_expansion_channels,
@@ -73,32 +91,81 @@ class Bottleneck(nn.Module):
             stride=stride,
             padding=1,
         )
+        self.bn2 = (
+            nn.BatchNorm2d(pre_expansion_channels) if use_batchnorm else nn.Identity()
+        )
+
         self.conv3 = nn.Conv2d(
             pre_expansion_channels, pre_expansion_channels * expansion
         )
-        
+        self.bn3 = (
+            nn.BatchNorm2d(pre_expansion_channels * expansion)
+            if use_batchnorm
+            else nn.Identity()
+        )
+
         self.relu = nn.ReLU()
-        
+
         # skip connection follows conv1
-        self.shortcut = nn.Conv2d(in_channels, pre_expansion_channels * expansion, kernel_size=1, stride=stride)
+        self.shortcut = nn.Conv2d(
+            in_channels,
+            pre_expansion_channels * expansion,
+            kernel_size=1,
+            stride=stride,
+        )
 
     def forward(self, x: torch.Tensor):
-        out_skip = self.conv1(x)
-        out_skip = self.relu(out_skip)
-        
-        # residual connection occurs after channel expansion
+        out = self.conv1(x)
+        out = self.relu(out)
         out = self.conv2(out_skip)
         out = self.relu(out)
         out = self.conv3(out)
-        
-        out += self.shortcut(out_skip)
+
+        out += self.shortcut(x)
         return self.relu(out)
 
+
 class ResNet(nn.Module):
-    def __init__(self) -> None:
+    def __init__(self, input_channels=64, use_batchnorm=True) -> None:
         super().__init__()
 
-    def forward(self):
+        # Pre-residual conv layer
+        self.conv1 = nn.Conv2d(3, input_channels, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(input_channels) if use_batchnorm else nn.Identity()
+
+        # Residual blocks
+        self.layer1 = self._create_layer(ResidualBlock, 64, num_blks=2, stride=1)
+        self.layer2 = self._create_layer(Bottleneck, 128, num_blks=2, stride=2)
+        self.layer3 = self._create_layer(Bottleneck, 256, num_blks=2, stride=2)
+        self.layer4 = self._create_layer(Bottleneck, 512, num_blks=2, stride=2)
+
+        # Linear output
+        self.readout = nn.Sequential(
+            [
+                nn.AdaptiveAvgPool2d((1, 1)),
+                nn.Flatten(),
+                nn.Linear(512, config.num_classes),
+            ]
+        )
+
+    def _create_layer(
+        self,
+        block: Type[ResidualBlock | Bottleneck],
+        channels: int,
+        num_blks=1,
+        stride=1,
+    ):
+        # Assign correct strides to preserve channels
+        stride_pattern = [stride] + [1] * (num_blks - 1)
+        layers = []
+        
+        in_channels = self.in_channels
+        
+        for stride in stride_pattern:
+            layers.append(block(in_channels, channels, stride=stride))
+            in_channels = channels * block.expansion
+
+    def forward(self, x: torch.Tensor):
         pass
 
 
